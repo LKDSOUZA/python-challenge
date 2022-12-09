5771838ecde0a8dd24d06a49e6ee81e5780d3cf0
abc <abc@example.com>
2022-12-09 14:24:25 +1100

    Updates to PyBank and PyPoll main.py files

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -1,10 +1,25 @@
 import os
 import csv
 
+months = []
+profitandloss = []
+total_pnl = []
+total_months = []
+
 csvpath = os.path.join('Resources/python-challenge/PyBank/Resources', 'budget_data.csv')
 
 with open(csvpath, 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+    
+    csv_data = csv.reader(csv_file)
+    csv_header = next(csv_data)
+    
 
-    for row in csv_reader:
-        print(row)
+    
+    for row in csv_data:
+        months.append(f"{row[0]}")
+        profitandloss.append(f"{row[1]}") 
+
+total_months += {len(months)}
+
+print(total_months)
+    
--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -7,4 +7,5 @@ with open(csvpath, 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     for row in csv_reader:
-        print(row)
+        my_list = [n for n in row]
+        print(my_list)
