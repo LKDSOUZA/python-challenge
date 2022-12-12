9c8af7bf8fcf880243e5b80f2f66c8c6f60dc4a6
abc <abc@example.com>
2022-12-12 14:57:19 +1100

    updates to pybank main.py and creation of analysis folder w/ txt file

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -2,24 +2,62 @@ import os
 import csv
 
 months = []
-profitandloss = []
-total_pnl = []
+pnl = []
 total_months = []
+
 
 csvpath = os.path.join('Resources/python-challenge/PyBank/Resources', 'budget_data.csv')
 
 with open(csvpath, 'r') as csv_file:
     
-    csv_data = csv.reader(csv_file)
+    csv_data = csv.reader(csv_file, delimiter=',')
     csv_header = next(csv_data)
     
 
     
     for row in csv_data:
         months.append(f"{row[0]}")
-        profitandloss.append(f"{row[1]}") 
-
+        pnl.append(int(row[1])) 
+    
 total_months += {len(months)}
 
-print(total_months)
+total = []
+total = 0
+
+for i in range(0,len(pnl)):
+    total = total + pnl[i]
+
+results = {
+    "Total Months" :total_months,
+    "Total $":total
+}
+
+print(results)
+print("Financial Analysis")
+print("--------------------------------")
+for i in results:
+    print(f"{i}: {results[i]}")
+
+
+
+
+
+
+
+
+outpath = os.path.join("Resources/python-challenge/PyBank/Resources/analysis", "financial_analysis.txt")
+
+with open(outpath, 'w') as output:
     
+    writer = csv.writer(output)
+    writer.writerow(["Financial Analysis"])
+    writer.writerow(["----------------------------------"])
+
+    writer.writerow([results])
+
+
+
+
+
+
+
